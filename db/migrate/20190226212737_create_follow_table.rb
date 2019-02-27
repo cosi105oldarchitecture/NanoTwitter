19e2d7409ef0f2c4039ede21d2d202f3996d0fc6
Ari Carr <acarr@brandeis.edu>
2019-02-27 15:50:32 -0500

    Fixed typo

--- db/migrate/20190226212737_create_follow_table.rb
+++ db/migrate/20190226212737_create_follow_table.rb
@@ -1,5 +1,5 @@
 # Initializes db's Follow table
-class CreateFolllowTable < ActiveRecord::Migration[5.2]
+class CreateFollowTable < ActiveRecord::Migration[5.2]
   def change
     create_table :follows do |t|
       t.integer :follower_id
