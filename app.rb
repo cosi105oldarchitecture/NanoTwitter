97d03b2b02909cad939e40c67d35eadb5638753b
Ari Carr <acarr@brandeis.edu>
2019-03-14 16:56:18 -0400

    Separated test API routes

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'activerecord-import'
 require 'sinatra/flash'
-require 'yaml'
 require 'bcrypt'
-require 'faker'
 require 'newrelic_rpm'
 
 unless Sinatra::Base.production?
@@ -20,11 +18,10 @@ require_relative 'lib/authentication'
 require_relative 'lib/register'
 require_relative 'lib/helpers'
 require_relative 'version'
-require_relative 'lib/seeds_helper'
-
 
 ENV['APP_ROOT'] = settings.root
 Dir["#{ENV['APP_ROOT']}/models/*.rb"].each { |file| require file }
+Dir["#{ENV['APP_ROOT']}/routes/*.rb"].each { |file| require file }
 
 # Expire sessions after ten minutes of inactivity
 TEN_MINUTES = 60 * 10
@@ -148,45 +145,4 @@ get '/tweets' do
   # user = User.find(1000) #REMOVE
   @timeline = user.timeline_tweets
   erb :tweets
-end
-
-post '/test/reset/all' do
-  delete_all
-  seed_users(nil)
-  seed_follows(nil)
-  seed_tweets(nil)
-  seed_testuser
-end
-
-post '/test/reset' do
-  users = params[:users]
-  tweets = params[:tweets]
-  delete_all
-  seed_users(users.to_i)
-  seed_follows(users.to_i)
-  seed_tweets(tweets.to_i)
-
-
-  seed_testuser
-end
-
-post '/test/user/:userid/tweets' do
-  userid = params[:userid]
-  n = params[:count].to_i
-  (0..n-1).each do |i|
-    t = Faker::Twitter.status
-    Tweet.create(author_id: userid.to_i, body: t["text"], created_on: t["created_at"])
-  end
-end
-
-
-# One page "report":
-# How many users, follows, and tweets are there
-# What is the TestUser's id
-get '/test/status' do
-  @users_num = User.count
-  @follow_num = Follow.count
-  @tweet_num = Tweet.count
-  @testuser_id = User.find_by(name: 'testuser').id
-  erb :report
 end
